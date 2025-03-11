caa236f71b96d3b260ad19d107fe4a12696a8696
ChasangBhutia <chasangtsering@gmail.com>
2025-03-11 23:33:45 +0530

    add local storage to store notes

--- src/App.js
+++ src/App.js
@@ -1,46 +1,58 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import InputArea from './Components/InputArea/InputArea';
 import ItemCard from './Components/ItemCard/ItemCard'
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 
+const getLocalStorage = ()=>{
+  const storedNotes = localStorage.getItem('notes');
+  if(storedNotes) return JSON.parse(localStorage.getItem('notes'));
+}
+
 function App() {
 
-  const [notes, setNotes] = useState([])
+  const [notes, setNotes] = useState(getLocalStorage)
+  
 
-  function addItem(newNote){
-    setNotes(prevValue=>{
-      return[...prevValue, newNote]
+  useEffect(()=>{
+    localStorage.setItem('notes', JSON.stringify(notes))
+
+  },[notes])
+  
+    
+  
+  
+  
+  function addItem(newNote) {
+    setNotes(prevValue => {
+      return [...prevValue, newNote]
     })
   }
 
-  const onRemove = (itemIndex)=>{
-    setNotes(prevValue=>{
-      return[...prevValue.filter((item,index)=>{       
-        return(index!==itemIndex);
+  const onRemove = (itemIndex) => {
+
+    setNotes(prevValue => {
+      return [...prevValue.filter((item, index) => {
+        return (index !== itemIndex);
       })]
     })
-    
-    
   }
-
-
 
   return (
 
-   <div className="app">
-    <Navbar/>
-    <InputArea onAdd={addItem}/>
-    <div className='itemContainer'>
-    {notes.map((item,index)=>{
-      return(
-        <ItemCard title={item.title} content={item.content} key={index} id={index} removeItem={onRemove}/>
-      )
-    })}
+    <div className="app">
+      <Navbar />
+      <InputArea onAdd={addItem} />
+      <div className='itemContainer'>
+        {notes.map((item, index) => {
+          return (
+            <ItemCard title={item.title} content={item.content} key={index} id={index} removeItem={onRemove} />
+          )
+        })}
+      </div>
+      <Footer />
     </div>
-    <Footer/>
-   </div>
   );
 }
 
